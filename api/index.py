971035b7329a3cc309d804be5b38e81db3ef8511
Kalila Ingco <103094824+kalilaingco@users.noreply.github.com>
2025-08-28 02:15:33 -0700

    gunicorn

--- api/index.py
+++ api/index.py
@@ -406,4 +406,6 @@ def migrate_sqlite_to_postgres():
 app = app
 
 
-app.run(debug=True)
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
